a18e21b859c36d7be6c58d3bedab05e66f515ace
seragkh21-sudo <seragkh21@gmail.com>
2026-09-09 13:25:50 +0300

    chore(android): bump widget app to 0.3.1

--- android-widget/app/build.gradle.kts
+++ android-widget/app/build.gradle.kts
@@ -7,8 +7,8 @@ android {
         applicationId = "com.serag.os"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.3.0"
+        versionCode = 4
+        versionName = "0.3.1"
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
