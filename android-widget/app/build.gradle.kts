fe7ac1686d7168be1c81d7c0d1d5b4afb1b14395
seragkh21-sudo <seragkh21@gmail.com>
2026-09-09 04:29:52 +0300

    chore: bump Android widget version

--- android-widget/app/build.gradle.kts
+++ android-widget/app/build.gradle.kts
@@ -1,29 +1,15 @@
-plugins {
-    id("com.android.application")
-}
+plugins { id("com.android.application") }
 
 android {
     namespace = "com.serag.os"
     compileSdk = 35
-
     defaultConfig {
         applicationId = "com.serag.os"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 3
+        versionName = "0.3.0"
     }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
